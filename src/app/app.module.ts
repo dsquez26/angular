d1ed258830c1ed79bee02c6bd184d3ba96b96094
David Vazquez <davidvazquez1026@gmail.com>
2019-03-12 14:58:57 -0500

    DLV-0001 added pic of J

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AssistComponent } from './assist/assist.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssistComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
