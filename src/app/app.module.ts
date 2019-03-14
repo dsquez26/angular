8938619cbc4088faf5142083f9e40334c031ec17
David Vazquez <davidvazquez1026@gmail.com>
2019-03-14 18:42:21 -0500

    started basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { AssistComponent } from './assist/assist.component';
 import { LoginComponent } from './login/login.component';
+import { LandingComponent } from './landing/landing.component';
+
+import { Router } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+  },
+  {
+    path: 'login',
+    component: LoginComponent  
+  },
+  {
+    path: 'landing',
+    component: LandingComponent
+  },
+  {
+    path: 'assist',
+    component: AssistComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     AssistComponent,
-    LoginComponent
+    LoginComponent,
+    LandingComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Router
   ],
   providers: [],
   bootstrap: [AppComponent]
